487d2f5f2831d2e6ace07c41e788a26cfea77fa0
scorpion1 <shgr8rb@gmail.com>
2015-09-23 11:58:54 +0500

    fix

--- app/src/main/java/com/byteshaft/ftpixer/SetUpDetails.java
+++ app/src/main/java/com/byteshaft/ftpixer/SetUpDetails.java
@@ -22,18 +22,6 @@ public class SetUpDetails extends AppCompatActivity {
         RadioGroup dataUsageGroup = (RadioGroup) findViewById(R.id.data_uage_radiogroup);
         mWifi = (RadioButton) findViewById(R.id.wifi_radio_button);
         mMobileData = (RadioButton) findViewById(R.id.mobile_data_radio_button);
-        dataUsageGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (mWifi.isChecked() && isWifiConnected()) {
-                    System.out.println("wifi");
-
-                } else if (mMobileData.isChecked() && isMobileDataConnected()) {
-
-                    System.out.println("data");
-                }
-            }
-        });
     }
 
     private boolean isWifiConnected() {
